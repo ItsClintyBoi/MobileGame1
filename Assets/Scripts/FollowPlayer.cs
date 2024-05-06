b9748c4042f2f3d5df42dc381e3d5bb3060009e5
Clinty <itsclintyboi@gmail.com>
2024-05-06 14:29:41 -0600

    ahhhh

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -34,8 +34,6 @@ public class FollowPlayer : MonoBehaviour
 
         if (playerScript.cameraValues[(int)playerScript.CurrentGamemode] > 10)
             FreeCam(firstFrame);
-        else
-            StaticCam(playerScript.cameraValues[(int)playerScript.CurrentGamemode], playerScript.yPosLastPortal, firstFrame);
 
         Background.localPosition = new Vector3((-player.position.x * 0.5f) + Mathf.Floor(player.transform.position.x / 96) * 48, 2.2f, 10);
         transform.position = newVector;
@@ -44,7 +42,7 @@ public class FollowPlayer : MonoBehaviour
 
     void FreeCam(bool doInstantly)
     {
-       //GroundCamera.position = Vector3.Lerp(new Vector3(0, GroundCamera.position.y), Vector3.up * cameraOffset, 20) + Vector3.right * (Mathf.Floor(player.transform.position.x / 5) * Time.deltaTime);
+        //GroundCamera.position = Vector3.Lerp(new Vector3(0, GroundCamera.position.y), Vector3.up * cameraOffset, 20) + Vector3.right * (Mathf.Floor(player.transform.position.x / 5) * Time.deltaTime);
 
         if (Vector2.Distance(TopGround.localPosition, Vector3.up * 20f) < 0.3f)
             TopGround.gameObject.SetActive(false);
@@ -60,22 +58,6 @@ public class FollowPlayer : MonoBehaviour
     Vector3 InterpolateVec3(Vector3 current, Vector3 target, float speed)
     {
         return current + Vector3.Normalize(target - current) * Time.deltaTime * speed * Mathf.Clamp(Vector2.Distance(current, target), 0, 1);
-    }
-
-    void StaticCam(int CamSize, float yPosLastPortal, bool doInstantly)
-    {
-        TopGround.gameObject.SetActive(true);
-
-        //GroundCamera.position = Vector3.Lerp(new Vector3(0, GroundCamera.position.y), Vector3.up * Mathf.Clamp(yPosLastPortal - CamSize * 0.5f, cameraOffset.y, float.MaxValue), 20) + Vector3.right * (Mathf.Floor(player.transform.position.x / 5) * 5);
-       if (jump == true)
-        {
-             TopGround.transform.localPosition = InterpolateVec3(TopGround.transform.localPosition, Vector3.up * (4.5f + CamSize), 30);
-        }
-
-        if (!doInstantly)
-            newVector += Vector3.up * (5 + Mathf.Clamp(yPosLastPortal - CamSize * 0.5f, cameraOffset.y, 2048) - newVector.y - ((11 - CamSize) * 0.5f)) * Time.deltaTime / interpolationTime;
-        else
-            newVector += Vector3.up * (5 + Mathf.Clamp(yPosLastPortal - CamSize * 0.5f, cameraOffset.y, 2048) - ((11 - CamSize) * 0.5f));
     }
 }
 
--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -9,8 +9,6 @@ public class PortalScript : MonoBehaviour
     public bool gravity;
     public int State;
 
-    [SerializeField] PortalTextures portalTextures;
-
     SpriteRenderer frontTexture;
     SpriteRenderer backTexture;
 
@@ -18,37 +16,9 @@ public class PortalScript : MonoBehaviour
 
     void Start()
     {
-        //Refresh textures
-        SpriteRenderer[] Renderers = GetComponentsInChildren<SpriteRenderer>();
-        col = GetComponent<BoxCollider2D>();
 
-        if (Renderers != null)
-            frontTexture = GetComponent<SpriteRenderer>();
-        if (Renderers.Length > 1)
-            backTexture = GetComponentsInChildren<SpriteRenderer>()[1];
-
-        switch (State)
-        {
-            case 0:
-                col.size = portalTextures.SpeedSprites[(int)Speed].rect.size / portalTextures.SpeedSprites[(int)Speed].pixelsPerUnit;
-                col.offset = Vector2.zero;
-                frontTexture.sprite = portalTextures.SpeedSprites[(int)Speed];
-                backTexture.sprite = null;
-                break;
-            case 1:
-                col.size = new Vector2(2.6f, 1.35f);
-                col.offset = new Vector2(0, 0.16f);
-                frontTexture.sprite = portalTextures.GamemodeSpritesFront[(int)Gamemode];
-                backTexture.sprite = portalTextures.GamemodeSpritesBack[(int)Gamemode];
-                break;
-            case 2:
-                col.size = new Vector2(2.6f, 1.1f);
-                col.offset = new Vector2(0, 0.16f);
-                frontTexture.sprite = portalTextures.GravityPortalSpritesFront[(int)Gamemode];
-                backTexture.sprite = portalTextures.GravityPortalSpritesBack[(int)Gamemode];
-                break;
-        }
     }
+
 
     public void initiatePortal(Movement movement)
     {
