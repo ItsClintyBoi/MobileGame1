d066254aba09cf20a8eb7e1e6a7fcf6fb984c64a
Clinty <itsclintyboi@gmail.com>
2024-05-07 12:05:29 -0600

    lvl

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,15 +5,12 @@ using UnityEngine.UIElements;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public float topGroundOffset;
     //(3.4, -2.3)
     public Vector2 cameraOffset;
 
     public float interpolationTime = 0.1f;
 
     public Transform player;
-    public Transform GroundCamera;
-    public Transform TopGround;
     public Transform Background;
     public Movement playerScript;
 
@@ -23,8 +20,6 @@ public class FollowPlayer : MonoBehaviour
     bool firstFrame = true;
     private void Update()
     {
-        scrollSpeed = Time.deltaTime;
-        GroundCamera.localPosition = new Vector3((-player.position.x * 0.5f) + Mathf.Floor(player.transform.position.x / 96) * 25, 21.6f, 10);
 
     }
     void FixedUpdate()
@@ -42,17 +37,7 @@ public class FollowPlayer : MonoBehaviour
 
     void FreeCam(bool doInstantly)
     {
-        //GroundCamera.position = Vector3.Lerp(new Vector3(0, GroundCamera.position.y), Vector3.up * cameraOffset, 20) + Vector3.right * (Mathf.Floor(player.transform.position.x / 5) * Time.deltaTime);
-
-        if (Vector2.Distance(TopGround.localPosition, Vector3.up * 20f) < 0.3f)
-            TopGround.gameObject.SetActive(false);
-        if (TopGround)
-            TopGround.localPosition = Vector3.Lerp(TopGround.localPosition, Vector3.up * topGroundOffset, 100);
-
-        if (!doInstantly)
-            newVector += Vector3.up * (Mathf.Lerp(player.transform.position.y + 1.7f - newVector.y, -0.6f - newVector.y, (player.position.y <= 4.2f) ? 1 : 0)) * Time.deltaTime / interpolationTime;
-        else
-            newVector += Vector3.up * (player.transform.position.y + 1.7f);
+        
     }
 
     Vector3 InterpolateVec3(Vector3 current, Vector3 target, float speed)
